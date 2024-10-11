4a0f1e8e2fab949bd217283734518e831c3933eb
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-11 18:48:20 -0300

    Inclusão de informações de contato

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,22 @@
+function Contato() {
+  return (
+    <div className="container content">
+      <div>
+        <h3>Aplicação desenvolvida por:</h3>
+        <p>Anderson Amicuchi Machado</p>
+      </div>
+      <hr />
+      <div>
+        <h3>Dúvidas, sugestões e comentários:</h3>
+        <p>AndersonAmicuchi@gmail.com</p>
+      </div>
+      <hr />
+      <div>
+        <h3>Repositório Público:</h3>
+        <p>https://github.com/Amicuchi/OsPensadores</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contato;

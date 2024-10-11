489f819f5b47ea0366f436439ec3a9915b0701da
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-11 12:42:50 -0300

    Sobre o App : Desenvolvimento da página

---
+++ src/pages/SobreApp.jsx
@@ -0,0 +1,18 @@
+function SobreApp() {
+  return (
+    <div className="sobre-app-container">
+      <h1>Sobre o App</h1>
+      <p>
+        O aplicativo &apos;Pensadores e os Animais&apos; foi criado para promover a reflexão sobre a relação entre os seres humanos e os animais, baseando-se no livro &apos;Pensadores e a Proteção aos Animais&apos;.
+      </p>
+      <p>
+        Este aplicativo foi criado com o objetivo de promover a reflexão sobre a relação entre os pensadores e a proteção aos animais.
+        Aqui você encontrará frases, informações e curiosidades sobre diversos pensadores e animais.
+      </p>
+      <h2>Contato</h2>
+      <p>Email: contato@pensadoreseosanimais.com</p>
+    </div>
+  );
+}
+
+export default SobreApp;

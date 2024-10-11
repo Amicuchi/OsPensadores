16433f7bcf975df80e30a5107fb7a2bd9dfd3ee1
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-11 18:47:23 -0300

    App: Alteração de rotas e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Pensadores from './pages/Pensadores';
 import Animais from './pages/Animais';
-import SobreAutor from './pages/SobreAutor';
 import SobreApp from './pages/SobreApp';
+import Contato from './pages/Contato';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pensadores" element={<Pensadores />} />
         <Route path="/animais" element={<Animais />} />
-        <Route path="/sobre-autor" element={<SobreAutor />} />
         <Route path="/sobre-app" element={<SobreApp />} />
+        <Route path="/contato" element={<Contato />} />
       </Routes>
     </Router>
   );

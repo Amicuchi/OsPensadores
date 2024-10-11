6b45253fd19c149425300f5a3fab355757d20660
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-11 18:48:55 -0300

    Exclusão da página "Sobre o Autor" e criação da página "Sobre o App"

--- src/pages/SobreApp.jsx
+++ src/pages/SobreApp.jsx
@@ -1,16 +1,23 @@
 function SobreApp() {
   return (
-    <div className="sobre-app-container">
-      <h1>Sobre o App</h1>
-      <p>
-        O aplicativo &apos;Pensadores e os Animais&apos; foi criado para promover a reflexão sobre a relação entre os seres humanos e os animais, baseando-se no livro &apos;Pensadores e a Proteção aos Animais&apos;.
-      </p>
-      <p>
-        Este aplicativo foi criado com o objetivo de promover a reflexão sobre a relação entre os pensadores e a proteção aos animais.
-        Aqui você encontrará frases, informações e curiosidades sobre diversos pensadores e animais.
-      </p>
-      <h2>Contato</h2>
-      <p>Email: contato@pensadoreseosanimais.com</p>
+    <div className="container content">
+      
+      <h3>Sobre o App</h3>
+      <p>Este aplicativo foi inspirado no livro &quot;Pensadores e a Proteção aos Animais&quot; de autoria do meu amigo, Eurípedes Kühl. A quem dedico o aplicativo.</p>
+      <p>Aqui você encontrará frases, informações e curiosidades sobre diversos pensadores e animais.</p>
+      
+      <hr />
+      
+      <h3>Sobre o Livro</h3>
+      <p>O Livro &apos;Pensadores e a Proteção aos Animais&apos; foi criado para promover a reflexão sobre a relação entre os seres humanos e os animais.</p>
+      
+      <hr />
+      
+      <h3>Contato do Autor</h3>
+      <p>Nome: Eurípedes Kühl</p>
+      <p>Email: euripedeskuhl@gmail.com</p>
+      <p>Website: http://euripedeskuhl.blogspot.com/</p>
+
     </div>
   );
 }

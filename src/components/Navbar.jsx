9e160e0c51981e6a91f8e545636a974cf6c2cbf5
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-11 18:47:53 -0300

    Navbar: Alteração de rotas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     <nav className="navbar">
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/sobre-autor">Sobre o Autor</Link></li>
         <li><Link to="/sobre-app">Sobre o App</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
       </ul>
     </nav>
   );
